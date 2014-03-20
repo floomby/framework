ee6b657b4d282f626b716f3069d34b40cb666ad9
floomby <caboodlennm@gmail.com>
2014-03-20 15:17:07 -0600

    shell exec in the works

--- shell/shellexec.cpp
+++ shell/shellexec.cpp
@@ -1,19 +1,45 @@
 #include "sopar.h"
 
-void ShellExecute(const char *what)
+void ShellInit()
 {
     // Create a pipes for std{err,in,out}
+    HANDLE hStdInReadPipe;
+    HANDLE hStdInWritePipe;
     
+    HANDLE hStdOutReadPipe;
+    HANDLE hStdOutWritePipe;
+    
+    HANDLE hStdErrReadPipe;
+    HANDLE hStdErrWritePipe;
+    
+    LPSECURITY_ATTRIBUTES attrs;
+    
+    attrs.nLength = sizeof(LPSECURITY_ATTRIBUTES);
+    attrs.lpSecurityDescriptor = NULL;
+    attrs.bInheritHandle = TRUE;
+    
+    CreatePipe(&hStdInReadPipe,  &hStdInWritePipe,  &attrs, 0);
+    CreatePipe(&hStdOutReadPipe, &hStdOutWritePipe, &attrs, 0);
+    CreatePipe(&hStdErrReadPipe, &hStdErrWritePipe, &attrs, 0);
+    
+    // Duplicate the handles
+    HANDLE hProc = GetCurrentProcessId();
+    
+    DuplicateHandle(hProc, );
+    DuplicateHandle(hProc, );
+    DuplicateHandle(hProc, );
 
+    
+    
+}
+
+void ShellExecute(const char *what)
+{
     // Create an instance of cmd.exe to run the command
     
     
     
     // Buffer the pipes over the socket until termination
-
-
-    
-    // Close the pipes
     
     
     
@@ -21,4 +47,13 @@ void ShellExecute(const char *what)
     
     
     
+}
+
+
+
+void ShellDone()
+{
+    // Close the pipes
+
+
 }
