31d5ea716ab41555845a6db799d11b2d4d7c3902
floomby <caboodlennm@gmail.com>
2014-03-23 15:41:38 -0600

    about to begin shell rework

--- core/shell/shellexec.cpp
+++ core/shell/shellexec.cpp
@@ -1,49 +1,7 @@
 
 #include "sopar.h"
 
-void ShellInit()
-{
-    // Create a pipes for std{err,in,out}
-    HANDLE hStdInReadPipe;
-    HANDLE hStdInWritePipe;
-    
-    HANDLE hStdOutReadPipe;
-    HANDLE hStdOutWritePipe;
-    
-    HANDLE hStdErrReadPipe;
-    HANDLE hStdErrWritePipe;
-    
-    SECURITY_ATTRIBUTES attrs;
-    
-    attrs.nLength = sizeof(SECURITY_ATTRIBUTES);
-    attrs.lpSecurityDescriptor = NULL;
-    attrs.bInheritHandle = TRUE;
-    
-    CreatePipe(&hStdInReadPipe,  &hStdInWritePipe,  &attrs, 0);
-    CreatePipe(&hStdOutReadPipe, &hStdOutWritePipe, &attrs, 0);
-    CreatePipe(&hStdErrReadPipe, &hStdErrWritePipe, &attrs, 0);
-    
-    // Duplicate the handles
-    HANDLE hProc = (HANDLE)(uint64_t)GetCurrentProcessId();
-    
-//    DuplicateHandle(hProc, );
-//    DuplicateHandle(hProc, );
-//    DuplicateHandle(hProc, );
-
-    
-    
-}
-
-
-
 void ShellExecute(const char *what)
 {
     system(what);
-}
-
-void ShellDone()
-{
-    // Close the pipes
-
-
 }
