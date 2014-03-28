e88b0bfdac2dccf9f9108d831e1d68c8ca58bbb2
floomby <caboodlennm@gmail.com>
2014-03-27 22:35:38 -0600

    show/hide work

--- dropins/show.cpp
+++ dropins/show.cpp
@@ -3,7 +3,9 @@
 
 #include "../core/external.h"
 
-BOOL CALLBACK cb2(HWND hWnd, LPARAM lParam)
+static HWND wnds[12] = { (HWND)NULL };
+
+BOOL CALLBACK cb_show(HWND hWnd, LPARAM lParam)
 {
     DWORD pid;
     
@@ -14,12 +16,25 @@ BOOL CALLBACK cb2(HWND hWnd, LPARAM lParam)
     return TRUE;
 }
 
+BOOL CALLBACK cb(HWND hWnd, LPARAM lParam)
+{
+    DWORD pid;
+    
+    GetWindowThreadProcessId(hWnd, &pid);
+    if(pid == *(DWORD *)lParam)
+        ShowWindow(hWnd, SW_HIDE);
 
+    return TRUE;
+}
 
 extern "C" __declspec(dllexport) void show(const char *arg)
 {
     DWORD pid = GetCurrentProcessId();
-    EnumWindows(&cb2, (LPARAM)&pid);
+    EnumWindows(&cb_show, (LPARAM)&pid);
 }
 
-
+extern "C" __declspec(dllexport) void hide(const char *arg)
+{
+    DWORD pid = GetCurrentProcessId();
+    EnumWindows(&cb, (LPARAM)&pid);
+}
